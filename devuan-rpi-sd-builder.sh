b0bf6972d7a6de8bdde73428cf46ad8c5274d376
emojifreak <53028623+emojifreak@users.noreply.github.com>
2021-02-04 08:01:51 +0900

    Update devuan-rpi-sd-builder.sh

--- devuan-rpi-sd-builder.sh
+++ devuan-rpi-sd-builder.sh
@@ -252,6 +252,7 @@ chroot ${MNTROOT} fake-hwclock save
 
 echo "rootfstype=$FSTYPE" >${MNTROOT}/etc/default/raspi-extra-cmdline
 echo 'disable_fw_kms_setup=1' >>${MNTROOT}/etc/default/raspi-firmware-custom
+echo 'disable_overscan=1' >>${MNTROOT}/etc/default/raspi-firmware-custom
 echo 'ROOTPART=LABEL=RASPIROOT' >>${MNTROOT}/etc/default/raspi-firmware
 if echo $MMARCH | fgrep -q arm64; then
   echo 'KERNEL_ARCH="arm64"' >>${MNTROOT}/etc/default/raspi-firmware
