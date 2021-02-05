c5080c5093726f1a46d6b05668e120760896b89d
emojifreak <53028623+emojifreak@users.noreply.github.com>
2021-02-05 15:43:18 +0900

    Update debian-rpi-sd-builder.sh

--- debian-rpi-sd-builder.sh
+++ debian-rpi-sd-builder.sh
@@ -332,7 +332,7 @@ systemd-nspawn -q -D ${MNTROOT} -a fake-hwclock save
 echo "rootfstype=$FSTYPE" >${MNTROOT}/etc/default/raspi-extra-cmdline
 echo 'disable_fw_kms_setup=1' >>${MNTROOT}/etc/default/raspi-firmware-custom
 echo '#disable_overscan=1' >>${MNTROOT}/etc/default/raspi-firmware-custom
-echo 'ROOTPART=LABEL=RASPIROOT' >>${MNTROOT}/etc/default/raspi-firmware
+echo 'ROOTPART="LABEL=RASPIROOT"' >>${MNTROOT}/etc/default/raspi-firmware
 if echo $MMARCH | fgrep -q arm64; then
   echo 'KERNEL_ARCH="arm64"' >>${MNTROOT}/etc/default/raspi-firmware
   echo 'hdmi_enable_4kp60=1' >>${MNTROOT}/etc/default/raspi-firmware-custom
