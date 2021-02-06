93b0e232d7518cdca6763092fe42d3cbef92c0b4
emojifreak <53028623+emojifreak@users.noreply.github.com>
2021-02-06 16:58:53 +0900

    Update root-setup.sh

--- root-setup.sh
+++ root-setup.sh
@@ -92,7 +92,7 @@ ACTION=="add|change", SUBSYSTEM=="block", KERNEL=="mmcblk[0-9]", ATTR{queue/sche
 EOF
 
 #echo 'CMA="256M@256M"' >>/etc/default/raspi-firmware 
-
+apt-get update
 apt-get -y --purge --autoremove --no-install-recommends install systemd-cron dbus-user-session libnss-systemd libpam-systemd
 apt-get -y --purge --autoremove --no-install-recommends install alsa-utils pciutils usbutils bluetooth  bluez bluez-firmware
 apt-get -y --purge --autoremove --no-install-recommends install desktop-base xfonts-base
