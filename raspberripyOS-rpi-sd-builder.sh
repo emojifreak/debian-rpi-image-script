9550481f4f96007f0abade76df0a70b001867896
emojifreak <53028623+emojifreak@users.noreply.github.com>
2021-03-26 15:56:19 +0900

    echo "127.0.1.1\t${YOURHOSTNAME}" >> ${MNTROOT}/etc/hosts

--- raspberripyOS-rpi-sd-builder.sh
+++ raspberripyOS-rpi-sd-builder.sh
@@ -179,6 +179,7 @@ mount -o async,discard,lazytime,noatime ${DEVFILE}${PARTCHAR}1 ${MNTROOT}/boot
 echo -n "Choose hostname: "
 read YOURHOSTNAME
 echo "$YOURHOSTNAME" >${MNTROOT}/etc/hostname
+echo "127.0.1.1\t${YOURHOSTNAME}" >> ${MNTROOT}/etc/hosts
 if [ ${FSTYPE} = btrfs ]; then
   cat >${MNTROOT}/etc/fstab <<EOF
 LABEL=RASPIROOT / ${FSTYPE} rw,async,lazytime,discard,compress-force=lzo 0 1
