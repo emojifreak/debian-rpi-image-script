ee44702750e2bdc8a6d758f158d700ea712016cf
emojifreak <53028623+emojifreak@users.noreply.github.com>
2021-07-31 17:37:31 +0900

    Update build-upstream-RT-raspi-kernel.sh

--- build-upstream-RT-raspi-kernel.sh
+++ build-upstream-RT-raspi-kernel.sh
@@ -252,7 +252,7 @@ fi
 if [ `dpkg --print-architecture` = arm64 ]; then
   make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabihf- $config
 else
-  make KCFLAGS="-pipe" $config
+  make KCFLAGS="-mtune=cortex-a53" $config
 fi
 
 if [ -t 0 ]; then
@@ -265,12 +265,12 @@ else
   set -e
 fi
 
-# KCFLAGS=-mtune=cortex-a72+crc for RPi4
-# KCFLAGS=-mtune=cortex-a53+crc for RPi3
-# KCFLAGS=-mtune=cortex-a7 for RPi2
+# KCFLAGS=-mcpu=cortex-a72+crc for RPi4
+# KCFLAGS=-mcpu=cortex-a53+crc for RPi3
+# KCFLAGS=-mcpu=cortex-a7 for RPi2
 
 if [ `dpkg --print-architecture` = arm64 ]; then
   make -j 3 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabihf- bindeb-pkg
 else
-  make -j 2 KCFLAGS="-pipe" bindeb-pkg
+  make -j 2 KCFLAGS="-mtune=cortex-a53" bindeb-pkg
 fi
