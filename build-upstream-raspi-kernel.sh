ecc325e807ce17720638f1d4641e93fdb4bb53dc
emojifreak <53028623+emojifreak@users.noreply.github.com>
2021-07-31 11:15:55 +0900

    cortex-a53

--- build-upstream-raspi-kernel.sh
+++ build-upstream-raspi-kernel.sh
@@ -252,7 +252,7 @@ fi
 if [ `dpkg --print-architecture` = arm64 ]; then
   make ARCH=arm64 LOCALVERSION=-preempt CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabihf- $config
 else
-  make LOCALVERSION=-preempt KCFLAGS=-mcpu=cortex-a7 $config
+  make LOCALVERSION=-preempt KCFLAGS=-mcpu=cortex-a53 $config
 fi
 
 if [ -t 0 ]; then
@@ -272,5 +272,5 @@ fi
 if [ `dpkg --print-architecture` = arm64 ]; then
   make -j 3 LOCALVERSION=-preempt ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabihf- bindeb-pkg
 else
-  make -j 2 LOCALVERSION=-preempt KCFLAGS=-mcpu=cortex-a7 bindeb-pkg
+  make -j 2 LOCALVERSION=-preempt KCFLAGS=-mcpu=cortex-a53 bindeb-pkg
 fi
